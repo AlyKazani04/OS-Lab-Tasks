a8d204b5e11585adf0d2d0160456545e2149c5d6
Aly <aly.kazani52@gmail.com>
2026-01-31 21:50:16 +0500

    Add lab2 cw task2

---
+++ lab2/cw/task2/a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char** argv)
+{
+    const char* filename = "file.txt";
+    char command[50] = "cat ";
+
+    FILE* f = fopen(filename, "r");
+
+    if(f == NULL)
+    {
+        perror("Error \'file_not_found\'");
+        exit(1);
+    }
+
+    strcat(command, filename);
+    system(command);
+
+    return 0;
+}
